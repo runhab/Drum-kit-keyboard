eb59d5e1dfb81cb681e9f5bb1d5ccf719f8e5ef1
Habtamu Fentahun <122001494+runhab@users.noreply.github.com>
2024-02-16 15:31:53 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 
-var x = document.getElementsByTagName("button").length;
+var x = document.querySelectorAll(".drum").length;
 var i = 0;
-var audiosound = ["sounds/crash.mp3","sounds/kick-bass.mp3","sounds/snare.mp3","sounds/tom-1.mp3","sounds/tom-2.mp3","sounds/tom-3.mp3","sounds/tom-4.mp3"];
-
 
 while (i < x) {
-    let j = i;
-    document.getElementsByTagName("button")[i].addEventListener("click", function () {
-        var audio = new Audio(audiosound[j]);
-        audio.play();
-        // console.log(this);
-
-        // alert(this.innerHTML);
-    })
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        playsound(buttonInnerHTML);
+        clickedbtn(buttonInnerHTML);
+    });
     i++;
+}
+
+
+
+document.addEventListener("keydown", function (event) {
+    playsound(event.key);
+    clickedbtn(event.key);
+});
+
+
+function clickedbtn(key){
+    var clickbutton = document.querySelector("." + key);
+    clickbutton.classList.add("pressed");
+    setTimeout(function () {
+        clickbutton.classList.remove("pressed");
+    }, 100);
+}
+function playsound(key) {
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        default:
+            break;
+    }
 }
